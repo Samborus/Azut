8b40a4c3c9f3563f95d22b25c37053ac10f86bc1
unknown <p.felczak@gmail.com>
2022-07-01 23:47:55 +0200

    new

---
+++ BE/Arrays/Controllers/Arrays.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arrays.Controllers
+{
+    public class Arrays : Controller
+    {
+        // GET: Arrays
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Arrays/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Arrays/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Arrays/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Arrays/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Arrays/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Arrays/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Arrays/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BE/Arrays/Controllers/WeatherForecastEndpointsClass.cs
@@ -0,0 +1,40 @@
+﻿using Arrays;
+namespace Arrays.Controllers;
+
+public static class WeatherForecastEndpointsClass
+{
+    public static void MapWeatherForecastEndpoints (this IEndpointRouteBuilder routes)
+    {
+        routes.MapGet("/api/WeatherForecast", () =>
+        {
+            return new [] { new WeatherForecast() };
+        })
+        .WithName("GetAllWeatherForecasts");
+
+        routes.MapGet("/api/WeatherForecast/{id}", (int id) =>
+        {
+            //return new WeatherForecast { ID = id };
+        })
+        .WithName("GetWeatherForecastById");
+
+        routes.MapPut("/api/WeatherForecast/{id}", (int id, WeatherForecast input) =>
+        {
+            return Results.NoContent();
+        })
+        .WithName("UpdateWeatherForecast");
+
+        routes.MapPost("/api/WeatherForecast/", (WeatherForecast model) =>
+        {
+            //return Results.Created($"/WeatherForecasts/{model.ID}", model);
+        })
+        .WithName("CreateWeatherForecast");
+
+        routes.MapDelete("/api/WeatherForecast/{id}", (int id) =>
+        {
+            if (id < 1)
+                return Results.NotFound();
+            return Results.Ok(new WeatherForecast { ID = id });
+        })
+        .WithName("DeleteWeatherForecast");  
+    }
+}
